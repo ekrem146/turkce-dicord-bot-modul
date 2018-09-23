cf3088b7f6dc07e4f769c390da4f59465f0a5f77
ekrem146 <38163905+ekrem146@users.noreply.github.com>
2018-09-23 17:15:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+    const Discord = require('discord.js');
+    const client = new Discord.Client();  
+  client.on('ready', () => {
+   console.log(`Bot Giriş Yaptı ${client.user.tag}!`);
+ });
+    module.exports.soru_cevap = function(soru,cevap){
+
+    client.on('message', msg => {
+      if (msg.content === soru) {
+        msg.reply(cevap);
+      }
+    });  
+
+    
+  
+    
+
+}
+
+module.exports.token = function(token){
+ client.login(token);   
+}
 
